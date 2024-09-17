07a520a7bcdea807fa81ac9effb30837e7af8d4c
lenin cruz mayo <40728531+lenin-developer@users.noreply.github.com>
2024-09-16 18:10:27 -0600

    add: vue-ignore fix

--- src/components/form/VButton/VButtonProps.d.ts
+++
@@ -1,6 +0,0 @@
-import type { ButtonHTMLAttributes } from 'vue'
-export type VButtonProps = {
-    text: string,
-    shape?: "default" | "circle" | "round",
-    size?: "xs" | "sm" | "md" | "lg",
-} &  ButtonHTMLAttributes
